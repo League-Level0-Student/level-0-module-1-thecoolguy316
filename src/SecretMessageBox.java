5f50ae3d0a73d2b1fec80b4b2edff5740f3ce9c2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-31 13:26:12 -0700

    I finshed SecretMessageBox, The Riddler ip

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -16,16 +16,22 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
-	
+	public static void main(String[] args) {
 	// 1. Set a password in a String variable
-	
+	String why = "fortnite";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+String message = JOptionPane.showInputDialog("Make a secrete message, hint (fortnite)");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+String randomguy = JOptionPane.showInputDialog("Wat is the password so you can get the dub!");
 	// 4. If their guess matches the password, show them the secret message
-
-	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+if (randomguy.equals(why) ) {
+	JOptionPane.showMessageDialog(null, message);
 }
+	// 5. If the password does not match, pop-up "INTRUDER!!"
+else {
+	JOptionPane.showMessageDialog(null, "Intruder!!!! Intruder!!!! PEEEE PIIII PEE PII WOAHHHH WAOHH KRI KRI KRI KRI BEEP BEEP!!!");
+}
+}
+}
+
