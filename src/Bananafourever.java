c758950c786754affb642cb75eaca9e02f1a7c17
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-14 13:27:10 -0700

    Banana4eva, Myages, XGeeks

---
+++ src/Bananafourever.java
@@ -0,0 +1,9 @@
+
+
+public class Bananafourever {
+public static void main(String[] args) {
+	for (int i = 0; i < 100000000; i++) {
+System.out.println("Fallout 76");
+	}
+}
+}
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -15,11 +15,49 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-
+String Diego = "SUPER SUPER SUPER Good Luck";
+String Kevin = "invisible";
+String Yen = "Levatation";
+String Daniel = "SUPER SUPER SUPER Strong";
+String Tra = "The Brains";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String name = JOptionPane.showInputDialog("Enter a name that is in my table please i beg you to please this is my everything please.");
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
+if (name.equals("Diego")){
+	JOptionPane.showMessageDialog(null, Diego);
+}
+	
 
-	}
+
+
+if (name.equals("Kevin")) {
+	JOptionPane.showMessageDialog(null, Kevin);
+}
+	if (name.equals("Yen")) {
+	JOptionPane.showMessageDialog(null, Yen);
 }
 
+ if (name.equals("Daniel")) {
+ JOptionPane.showMessageDialog(null, Daniel);
+}
+if (name.equals("Tra")) {
+JOptionPane.showMessageDialog(null, Tra);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
+	
